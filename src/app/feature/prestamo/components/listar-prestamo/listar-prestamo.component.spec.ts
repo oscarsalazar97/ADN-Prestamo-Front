5211d7f770770de3ebf2e7a88e013bdae48fda7e
oscar.salazar <oscar.salazar@ceiba.com.co>
2022-06-01 20:43:03 -0500

    cambios con respecto a redux

--- src/app/feature/prestamo/components/listar-prestamo/listar-prestamo.component.spec.ts
+++ src/app/feature/prestamo/components/listar-prestamo/listar-prestamo.component.spec.ts
@@ -1,25 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { ListarPrestamoComponent } from './listar-prestamo.component';
+import { Prestamo } from './../../shared/model/prestamo';
+import { cold } from 'jasmine-marbles';
 
 describe('ListarPrestamoComponent', () => {
   let component: ListarPrestamoComponent;
   let fixture: ComponentFixture<ListarPrestamoComponent>;
+  let store: MockStore;
+  const listaPrestamos: Prestamo[] = [new Prestamo('1', '3', 'Cliente 1', 120000, 140000, 'SEMANAL', 'ACTIVO')];
+  const initialState = { data: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListarPrestamoComponent ]
+      declarations: [ ListarPrestamoComponent ],
+      providers: [
+        provideMockStore({initialState})
+      ]  
     })
     .compileComponents();
+
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarPrestamoComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('con elementos en la lista de prestamo', () => {
+    let consultarPrestamosExito = '[Prestamo Component] Consultar prestamos';
+    store.setState({type: consultarPrestamosExito, data: listaPrestamos});
+
+    const expected = cold('(prestamo|)', { prestamo: listaPrestamos });    
+
+    expect(store.select((state) => state.prestamo)).toBeObservable(expected);
   });
 });
--- src/app/feature/prestamo/shared/service/prestamo.service.spec.ts
+++ src/app/feature/prestamo/shared/service/prestamo.service.spec.ts
@@ -1,16 +1,45 @@
+import { HttpService } from 'src/app/core/services/http.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 
 import { PrestamoService } from './prestamo.service';
+import { Prestamo } from '../model/prestamo';
 
 describe('PrestamoService', () => {
+  let httpMock: HttpTestingController;
   let service: PrestamoService;
+  const apiEndpointPrestamosLista = `${environment.endpoint}/prestamos`;
+  //const apiEndpointPrestamo = `${environment.endpoint}/prestamo`;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers:[PrestamoService, HttpService]
+    });
+
+    httpMock = injector.inject(HttpTestingController);
     service = TestBed.inject(PrestamoService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    const prestamoService: PrestamoService = TestBed.inject(PrestamoService);
+    expect(prestamoService).toBeTruthy();
+  });
+
+  it('deberia listar prestamos', () => {
+    const dummyPrestamos = [
+      new Prestamo('1', '3', 'Cliente 1', 120000, 140000, 'SEMANAL', 'ACTIVO'),
+      new Prestamo('2', '3', 'Cliente 1', 130000, 150000, 'SEMANAL', 'ACTIVO')
+    ];
+
+    service.consultar().subscribe(prestamos => {
+      expect(prestamos.length).toBe(2);
+      expect(prestamos).toEqual(dummyPrestamos);
+    });
+    const req = httpMock.expectOne(apiEndpointPrestamosLista);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyPrestamos);
+
   });
 });
